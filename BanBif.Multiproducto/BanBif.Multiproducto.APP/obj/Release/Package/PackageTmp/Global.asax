a61621ebdef2ad85b6354567a2d196b9f790978f
Daniel Romero <croweloper@gmail.com>
2022-08-08 06:38:19 -0500

    Produccion

---
+++ BanBif.Multiproducto/BanBif.Multiproducto.APP/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BanBif.Multiproducto.APP.MvcApplication" Language="C#" %>
---
+++ BanBif.Multiproducto/BanBif.Multiproducto.APP/obj/Release/Package/PackageTmp/assets/mailmask.js
@@ -0,0 +1,11 @@
+﻿
+function MascaraAsteriscosMail(mail) {
+
+    let value = mail;;
+    let chars = 3; // Cantidad de caracters visibles
+
+    let res = value.replace(/[a-z0-9\-_.]+@/ig, (c) => c.substr(0, chars) + c.split('').slice(chars, -1).map(v => '*').join('') + '@')
+    
+    return res
+    
+}
